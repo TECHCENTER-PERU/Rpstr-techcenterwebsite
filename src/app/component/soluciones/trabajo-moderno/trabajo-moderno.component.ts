374ad114b1f4bf06c2067cfa492fd166db522ec1
BeaNoXG <bgonzalez@techcenterperu.com>
2025-05-29 01:24:06 -0500

    Trabajo Moderno responsivo

--- src/app/component/soluciones/trabajo-moderno/trabajo-moderno.component.ts
+++ src/app/component/soluciones/trabajo-moderno/trabajo-moderno.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarColorComponent } from '../../navbar-color/navbar-color.component';
-
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-trabajo-moderno',
-  imports: [NavbarColorComponent],
+  imports: [NavbarColorComponent, FooterComponent],
   templateUrl: './trabajo-moderno.component.html',
   styleUrl: './trabajo-moderno.component.css'
 })
