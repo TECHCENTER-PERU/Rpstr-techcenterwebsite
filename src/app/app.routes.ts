bf247fc7859886d114fa94b9be332128acbb11e0
BeaNoXG <bgonzalez@techcenterperu.com>
2025-05-29 09:59:52 -0500

    Vista de Aplicaciones Empresariales responsivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { SolucionesComponent } from './component/soluciones/soluciones.component';
 import { TrabajoModernoComponent } from './component/soluciones/trabajo-moderno/trabajo-moderno.component';
+import { AplicacionesEmpresarialesComponent } from './component/soluciones/aplicaciones-empresariales/aplicaciones-empresariales.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'soluciones', component: SolucionesComponent},
-    {path: 'trabajo-moderno', component: TrabajoModernoComponent}
+    {path: 'trabajo-moderno', component: TrabajoModernoComponent},
+    {path: 'aplicaciones-empresariales', component: AplicacionesEmpresarialesComponent}
+
 
 ];
---
+++ src/app/component/soluciones/aplicaciones-empresariales/aplicaciones-empresariales.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarColorComponent } from '../../navbar-color/navbar-color.component';
+import { FooterComponent } from '../../footer/footer.component';
+
+@Component({
+  selector: 'app-aplicaciones-empresariales',
+  imports: [NavbarColorComponent, FooterComponent],
+  templateUrl: './aplicaciones-empresariales.component.html',
+  styleUrl: './aplicaciones-empresariales.component.css'
+})
+export class AplicacionesEmpresarialesComponent {
+
+}
