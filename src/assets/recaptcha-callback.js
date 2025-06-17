5aab5ac23b13712c84b6a965ce423c5c237d200f
BeaNoXG <bgonzalez@techcenterperu.com>
2025-06-17 12:58:37 -0500

    captcha

--- src/assets/recaptcha-callback.js
+++ src/assets/recaptcha-callback.js
@@ -1,6 +1,9 @@
 window.onRecaptchaLoadCallback = function () {
+  console.log('🟢 reCAPTCHA cargado por Google');
   const ngZone = window.ngZoneRef;
   if (ngZone && window.recaptchaRenderCallback) {
     ngZone.run(() => window.recaptchaRenderCallback());
+  } else {
+    console.warn('⚠️ No se encontró NgZone o recaptchaRenderCallback');
   }
 };
